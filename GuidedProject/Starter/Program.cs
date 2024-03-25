b91af3b4117af5ad85b5368a22534ea1d7f692e0
jra <julien.gault.50@gmail.com>
2024-03-25 16:22:02 +0100

    convert MakeChange method to void method

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -79,22 +79,14 @@ while (transactions > 0)
     try
     {
         // MakeChange manages the transaction and updates the till 
-        string transactionMessage = MakeChange(itemCost, cashTill, paymentTwenties, paymentTens, paymentFives, paymentOnes);
+        MakeChange(itemCost, cashTill, paymentTwenties, paymentTens, paymentFives, paymentOnes);
+
+        Console.WriteLine($"Transaction successfully completed.");
+        registerCheckTillTotal += itemCost;
     }
     catch (InvalidOperationException e)
     {
         Console.WriteLine($"Could not complete transaction: {e.Message}");
-    }
-
-    // Backup Calculation - each transaction adds current "itemCost" to the till
-    if (transactionMessage == "transaction succeeded")
-    {
-        Console.WriteLine($"Transaction successfully completed.");
-        registerCheckTillTotal += itemCost;
-    }
-    else
-    {
-        Console.WriteLine($"Transaction unsuccessful: {transactionMessage}");
     }
 
     Console.WriteLine(TillAmountSummary(cashTill));
@@ -119,10 +111,8 @@ static void LoadTillEachMorning(int[,] registerDailyStartingCash, int[] cashTill
 }
 
 
-static string MakeChange(int cost, int[] cashTill, int twenties, int tens = 0, int fives = 0, int ones = 0)
+static void MakeChange(int cost, int[] cashTill, int twenties, int tens = 0, int fives = 0, int ones = 0)
 {
-    string transactionMessage = "";
-
     cashTill[3] += twenties;
     cashTill[2] += tens;
     cashTill[1] += fives;
@@ -166,11 +156,6 @@ static string MakeChange(int cost, int[] cashTill, int twenties, int tens = 0, i
 
     if (changeNeeded > 0)
         throw new InvalidOperationException("InvalidOperationException: The till is unable to make the correct change.");
-
-    if (transactionMessage == "")
-        transactionMessage = "transaction succeeded";
-
-    return transactionMessage;
 }
 
 static void LogTillStatus(int[] cashTill)
